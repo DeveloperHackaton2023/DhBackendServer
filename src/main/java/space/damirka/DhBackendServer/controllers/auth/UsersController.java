9c63f7959d2feef06e5b7b36faf280be8fed7594
Damirka1 <hamitov.damir3101@icloud.com>
2023-03-25 17:46:21 +0600

    Added method that returns all roles of user

--- src/main/java/space/damirka/DhBackendServer/controllers/auth/UsersController.java
+++ src/main/java/space/damirka/DhBackendServer/controllers/auth/UsersController.java
@@ -1,5 +1,8 @@
 package space.damirka.DhBackendServer.controllers.auth;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import space.damirka.DhBackendServer.models.requests.UsersRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import space.damirka.DhBackendServer.services.auth.AuthUsersService;
 import space.damirka.DhBackendServer.models.auth.Users;
 
+import java.security.Principal;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -28,9 +34,18 @@ public class UsersController {
     }
 
     @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
-    @PostMapping("get/")
+    @PostMapping("add/")
     public Users getUsers(@RequestBody UsersRequest user) {
         return authUsersService.AddUser(user);
+    }
+
+    @GetMapping("get/roles/")
+    public ResponseEntity<?> getUser(Principal principal) {
+        if(principal instanceof UsernamePasswordAuthenticationToken) {
+            return ResponseEntity.ok(((UsernamePasswordAuthenticationToken) principal)
+                    .getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
+        }
+        return ResponseEntity.badRequest().body("You are not authorized");
     }
 
 }
